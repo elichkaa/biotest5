3fe9b9d4e9b3019b14ec626395125f2bb98f3219
elichka <ed44362072@edu.mon.bg>
2021-04-17 20:44:02 +0300

    Add all presentations (pak s frto @lubi222)

--- biotest5/src/App.js
+++ biotest5/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Presentation1 from "./presentations/Presentation1";
+import Presentation2 from "./presentations/Presentation2";
+import Presentation3 from "./presentations/Presentation3";
+import Presentation4 from "./presentations/Presentation4";
+import Presentation5 from "./presentations/Presentation5";
+import Presentation6 from "./presentations/Presentation6";
+import Presentation7 from "./presentations/Presentation7";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Biotest pri vikito 5</h1>
+			<h3>hora, kak beshe shortcuta za tursene?</h3>
+			<h3>ctrl + f hora!</h3>
+			<Presentation1></Presentation1>
+			<Presentation2></Presentation2>
+			<Presentation3></Presentation3>
+			<Presentation4></Presentation4>
+			<Presentation5></Presentation5>
+			<Presentation6></Presentation6>
+			<Presentation7></Presentation7>
+		</div>
+	);
 }
 
 export default App;
